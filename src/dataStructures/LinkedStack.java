72bd72f3edb156b079b62604cc3e4e2c4c0573cd
ulambayar0511 <ulmaaulambayar4@gmail.com>
2021-10-10 11:41:25 +0800

    lab3 ymarch bsn ajildag ym hiiw

--- src/dataStructures/LinkedStack.java
+++ src/dataStructures/LinkedStack.java
@@ -5,67 +5,71 @@ package dataStructures;
 
 import java.util.EmptyStackException;
 
-public class LinkedStack implements Stack
-{
-   // data members
-   protected ChainNode topNode;
+public class LinkedStack implements Stack {
+    // data members
+    protected ChainNode topNode;
+    protected int size = 0;
 
-   // constructors
-   /** create an empty stack */
-   public LinkedStack(int initialCapacity)
-   {
-       // the default initial value of topNode is null
-   }
+    // constructors
+    /** create an empty stack */
+    public LinkedStack(int initialCapacity) {
+        size = initialCapacity;
+        // the default initial value of topNode is null
+    }
 
-   public LinkedStack()
-      {this(0);}
+    public LinkedStack() {
+        this(0);
+    }
 
-   // methods
-   /** @return true iff stack is empty */
-   public boolean empty()
-      {return topNode == null;}
+    // methods
+    /** @return true iff stack is empty */
+    public boolean empty() {
+        return topNode == null;
+    }
 
+    /**
+     * @return top element of stack
+     * @throws EmptyStackException when the stack is empty
+     */
+    public Object peek() {
+        if (empty())
+            throw new EmptyStackException();
+        return topNode.element;
+    }
 
-   /** @return top element of stack 
-     * @throws EmptyStackException when the stack is empty */
-   public Object peek()
-   {
-      if (empty())
-         throw new EmptyStackException();
-      return topNode.element;
-   }
+    /** add theElement to the top of the stack */
+    public void push(Object theElement) {
+        topNode = new ChainNode(theElement, topNode);
+        size++;
+    }
 
-   /** add theElement to the top of the stack */
-   public void push(Object theElement)
-      {topNode = new ChainNode(theElement, topNode);}
+    /**
+     * remove top element of stack and return it
+     * 
+     * @throws EmptyStackException when the stack is empty
+     */
+    public Object pop() {
+        if (empty())
+            throw new EmptyStackException();
+        Object topElement = topNode.element;
+        topNode = topNode.next;
+        size--;
+        return topElement;
+    }
 
-   /** remove top element of stack and return it
-     * @throws EmptyStackException when the stack is empty */
-   public Object pop()
-   {
-      if (empty())
-         throw new EmptyStackException();
-      Object topElement = topNode.element;
-      topNode = topNode.next;
-      return topElement;
-   }
-   
-   /** test program */
-   public static void main(String [] args)
-   {  
-      LinkedStack s = new LinkedStack();
-      // add a few elements
-      s.push(new Integer(1));
-      s.push(new Integer(2));
-      s.push(new Integer(3));
-      s.push(new Integer(4));
+    /** test program */
+    public static void main(String[] args) {
+        LinkedStack s = new LinkedStack();
+        // add a few elements
+        s.push(new Integer(1));
+        s.push(new Integer(2));
+        s.push(new Integer(3));
+        s.push(new Integer(4));
 
-
-      // delete all elements
-      while (!s.empty())
-      {
-         System.out.println("Top element is " + s.peek());
-         System.out.println("Removed the element " + s.pop());
-      }
-   }  
+        // delete all elements
+        while (!s.empty()) {
+            System.out.println("Top element is " + s.peek());
+            System.out.println("Removed the element " + s.pop());
+        }
+    }
 }
